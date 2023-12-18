73485465d6e03bdfaa1fce7292ea0150b96d9e77
connect2local247 <connect2local247@gmail.com>
2023-12-18 17:09:04 +0530

    project setup

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Connect2Local - Home Page</title>
+    <?php
+        include "./asset/link/cdn-link.html";
+    ?>
 </head>
 <body>
-    
+    <header>    
+        <nav>
+            
+        </nav>
+    </header>
 </body>
+</html>
+
 </html>
