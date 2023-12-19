e863e2741c03685ca2eb1610ec89d3dc026b34a1
connect2local247 <connect2local247@gmail.com>
2023-12-19 17:06:19 +0530

    home page structure design

--- index.php
+++ index.php
@@ -4,16 +4,217 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connect2Local - Home Page</title>
+    <link rel="stylesheet" href="/asset/css/style.css">
     <?php
         include "./asset/link/cdn-link.html";
     ?>
 </head>
 <body>
+    <?php include "./component/navbar.php";?>  
     <header>    
-        <nav>
-            
-        </nav>
+        <section class="intro d-flex align-items-center p-3 " style="height:100vh;width:100%;background:url('/asset/image/background/home-bg2.jpg') no-repeat;background-size:100% 100%;'">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12 d-flex align-items-center">
+                    <div class="content p-5 ms-5 text-white">
+                        <h1 class="fs-2 fw-bold">Welcome to Connect2local</h1>
+                        <p class="fs-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam sint quia, sapiente, id corrupti dolores minima nostrum eligendi nam minus voluptate ea illum nemo eos?</p>
+                        <button class="btn bg-success bg-gradient text-white px-5 py-3">Get Started</button>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-12 d-flex justify-content-end">
+                    <img src="/asset/image/logo.png" alt="">
+                </div>
+
+            </div>
+        </div>
+        </section>
     </header>
+
+    <div class="container"> 
+        <section class="our-services p-5 my-5 mx-2 ">
+                <h1 class="fs-2 fw-bold text-center">Our Service</h1>
+                <div class="row">
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Advertising</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Contstruction</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Automobile</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Accessories</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Education</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Fashion</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Transportation</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Hotel</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-6 g-4">
+                        <div class="card" style="width:80%">
+                            <div class="card-content border border-dark rounded-1 shadow body-teriatory">
+                                <div class="card-body d-flex justify-content-center align-items-center" style="height:250px;">
+                                    <img src="/asset/image/category/advertising.png" alt="Advertising image" class="category-img ">
+                                </div>
+                                <div class="card-footer text-center border-top border-dark">
+                                    <a href="" class="nav-link">Electronic</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+
+        <section class="blog-overview">
+                <h1 class="fs-2 fw-bold text-center">Our Blog</h1>
+                <div class="row">
+                <div class="col-lg-4 col-6 g-4"> 
+                    <div class="card mb-4 blog-post">
+                        <div class="card-body">
+                            <img src="https://placehold.it/200x150" class="card-img-top" alt="Blog Post Image">
+                            </div>
+                        <div class="card-footer">
+                            <h5 class="card-title">Blog Post Title</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p class="card-text">
+                                <small class="text-muted">Posted on January 1, 2023 by <a href="#">Author Name</a></small>
+                            </p>
+                            <a href="#" class="btn btn-primary btn-sm">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-6 g-4"> 
+                    <div class="card mb-4 blog-post">
+                        <div class="card-body">
+                            <img src="https://placehold.it/200x150" class="card-img-top" alt="Blog Post Image">
+                            </div>
+                        <div class="card-footer">
+                            <h5 class="card-title">Blog Post Title</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p class="card-text">
+                                <small class="text-muted">Posted on January 1, 2023 by <a href="#">Author Name</a></small>
+                            </p>
+                            <a href="#" class="btn btn-primary btn-sm">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-6 g-4"> 
+                    <div class="card mb-4 blog-post">
+                        <div class="card-body">
+                            <img src="https://placehold.it/200x150" class="card-img-top" alt="Blog Post Image">
+                            </div>
+                        <div class="card-footer">
+                            <h5 class="card-title">Blog Post Title</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <p class="card-text">
+                                <small class="text-muted">Posted on January 1, 2023 by <a href="#">Author Name</a></small>
+                            </p>
+                            <a href="#" class="btn btn-primary btn-sm">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                
+                
+                </div>
+        </section>
+    </div>
+
+    <footer>
+        
+    
+    </footer>
 </body>
 </html>
 
