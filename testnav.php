a51c6e7a316198612e949f1381f58c2a960060dc
het <nayakhet18@gmail.com>
2023-12-29 14:26:37 +0530

    bhavesh

--- testnav.php
+++ testnav.php
@@ -15,6 +15,7 @@
         overflow-y: scroll !important;
     }
 } */
+
     </style>
 </head>
 <body>
@@ -36,27 +37,61 @@
                     </ul>
                 </div> -->
 
-                <div class="menu-icon">
-                    <i class="fa-solid fa-bars text-white fs-5"></i>
-                </div>
+ <!-- Menubar -->
+
+                <!-- <div class="menu-icon">
+                    <i class="fa-solid fa-bars text-white fs-5" id="menu-btn"></i>
+                    <script>
+                        menuButton=document.getElementById("menu-btn")
+                        menuButton.addEventListener("click" , function(){
+                            
+                        })
+                    </script>
+                </div> -->
                 <!-- <div class="register">
                     <button class="btn text-bg-primary bg-gradient px-4 py-2 rounded-pill user-auth-btn" onclick="location.href='/component/choice.php'">Sign Up</button>
                     <button class="btn  text-bg-primary bg-gradient px-4 py-2 rounded-pill user-auth-btn">Login</button>
                 </div> -->
             </div>
-    </nav>
+            <i class="text-white px-5 fa-solid fa-bars" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"></i>
 
-    <div class="col-lg-3 col-md-5 col-12 bg-dark position-fixed p-3" style="height:100vh;z-index:10;" id="nav-animation">
-                <i class="fa-solid fa-xmark fs-5 position-absolute end-0 top-2 text-white px-4"></i>
-                <div class="brand-logo d-flex justify-content-center">    
-                    <a href="index.php" class="nav-link"><img src="/asset/image/logo.png" alt="This is logo" height="200" width="200"></a>        
-                </div>
-
-                <div class="register d-flex justify-content-center" style="gap:15px">
+<div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+    <i class="fa-solid fa-xmark text-white" data-bs-dismiss="offcanvas" aria-label="Close" style="cursor:pointer"></i>
+  </div>
+  <div class="offcanvas-body">
+  <div class="register d-flex justify-content-center" style="gap:15px" >
                     <button id="sign-up-btn" class="btn text-white border border-primary px-4 py-2 rounded-pill " onclick="location.href='/component/choice.php'" style="width:120px;height:45px">Sign Up</button>
                     <button id="login-btn" class="btn  border text-white border-danger px-4 py-2 rounded-pill " style="width:120px">Login</button>
                 </div>
-            <div class="menu d-flex justify-content-center  mt-5 ms-5">   
+                <div class="menu d-flex justify-content-center  mt-5 ms-5">   
+                    <ul class="list-unstyled d-flex flex-column" style="gap:15px;">
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fa-solid fa-house mx-1"></i> Home</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fa-solid fa-address-card mx-1"></i> About</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fas fa-cogs mx-1"></i> Service</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fas fa-phone mx-1"></i> Contact</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fas fa-pencil-alt mx-1"></i> Blog</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fas fa-question-circle mx-1"></i> Help</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fas fa-file-alt mx-1"></i> Term & Condition</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fas fa-shield-alt mx-1"></i> Privacy Policy</a></li>
+                    </ul>
+                </div>            
+  </div>
+</div>
+    </nav>
+
+    <!-- <div class="col-lg-3 col-md-5 col-12 bg-dark position-fixed p-3 d-flex flex-column align-items-center justify-content-center" style="height:100vh;z-index:10;" id="nav-animation">
+                <i class="fa-solid fa-xmark fs-5 position-absolute start-0 top-0 mt-4 ps-3 text-white px-4"></i>
+                <div class="brand-logo d-flex justify-content-center">    
+                    <a href="index.php" class="nav-link"><img src="/asset/image/logo.png" alt="This is logo" height="200" width="200"></a>        
+                </div> -->
+
+                <!-- <div class="register d-flex justify-content-center" style="gap:15px">
+                    <button id="sign-up-btn" class="btn text-white border border-primary px-4 py-2 rounded-pill " onclick="location.href='/component/choice.php'" style="width:120px;height:45px">Sign Up</button>
+                    <button id="login-btn" class="btn  border text-white border-danger px-4 py-2 rounded-pill " style="width:120px">Login</button>
+                </div> -->
+             <!-- <div class="menu d-flex justify-content-center  mt-5 ms-5">   
                     <ul class="list-unstyled d-flex flex-column" style="gap:15px;">
                         <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fa-solid fa-house mx-1"></i> Home</a></li>
                         <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fa-solid fa-address-card mx-1"></i> About</a></li>
@@ -68,7 +103,7 @@
                         <li class="nav-item"><a href="#" class="nav-link text-white fs-5"><i class="fas fa-shield-alt mx-1"></i> Privacy Policy</a></li>
                     </ul>
                 </div>
-    </div>
+    </div> --> 
 
 </body>
 </html>
