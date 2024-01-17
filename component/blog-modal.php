b7fa7ae18780288fd135fc3f80efa510d1f135ce
connect2local247 <connect2local247@gmail.com>
2024-01-17 20:55:31 +0530

    Blog Setup Design

---
+++ component/blog-modal.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="blogDetailModal" tabindex="-1" aria-labelledby="blogDetailModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header position-relative border-0">
+        <i class="fa-solid fa-xmark position-absolute end-0 me-2 p-2 fs-5" data-bs-dismiss="modal" aria-label="Close"></i>
+      </div>
+      <div class="modal-body">
+        <ul class="list-unstyled text-center">
+            <li class="list-item mt-2 shadow border p-2 rounded fs-5 d-flex align-items-center justify-content-center"> info</li>
+            <li class="list-item mt-2 shadow border p-2 rounded fs-5 d-flex align-items-center justify-content-center"> <i class="fa-solid fa-floppy-disk fs-4"></i>&nbsp;Save</li>
+            <li class="list-item mt-2 shadow border p-2 rounded fs-5 d-flex align-items-center justify-content-center">Block</li>
+            <li class="list-item mt-2 shadow border p-2 rounded fs-5 d-flex align-items-center justify-content-center">Report</li>
+            <li class="list-item"></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
--- testblog.php
+++ testblog.php
@@ -5,72 +5,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php include "./asset/link/cdn-link.html"; ?>
-    <style>
-        .collapsed-content {
-            display: none;
-        }
-    </style>
+    <link rel="stylesheet" href="/asset/css/style.css">
 </head>
-<body style="height:100vh;width:100%" class="d-flex align-items-center justify-content-center">
-        <div class="container">
-            <div class="card col-lg-4 col-md-5 col-12 text-bg-dark">
-                <div class="card-content">
-                    <div class="card-header border-bottom border-secondary">
-                        <img src="/asset/image/user/businessman.png" class="bg-light p-1 rounded-circle" style="height:50px;width:50px" alt="">
-                        <span class="ms-2 fs-5">bhavesh_1724</span>
-                    </div>
-                    <div class="card-body">
-                            <div class="card-img">
-                                    <img src="/asset/image/background/home-bg.jpg" style="height:100%;width:100%" class="rounded-2 mb-2" alt="">
+<body>
+    <div class="container">
+        <div class="card col-lg-3 col-md-5 col-12 border-0">
+            <div class="card-content border border-dark rounded shadow">
+                <div class="card-header d-flex justify-content-between px-3 align-items-center">
+                        <div class="user-data d-flex align-items-center" style="gap:10px">
+                            <img src="/asset/image/user/customer.png" alt="user image" style="height:50px;width:50px" class="rounded-circle bg-light">
+                            <span class="user-name fw-bold">bhavesh_1724</span>
+                        </div>
+                        <i class="fa-solid fa-ellipsis-vertical" data-bs-target="#blogDetailModal" data-bs-toggle="modal"></i>
+                        <?php include "./component/blog-modal.php"; ?>
+                </div>
+                <div class="card-body d-flex flex-column justify-content-center">
+                        <div class="content">
+                            <!-- <?php
+                                // functionality pending for image or video show content as blog
+                            ?> -->
+                            <img src="/asset/image/background/home-bg.jpg" class="rounded-2" style="height:100%;width:100%;" alt="">
+                        </div>
+                        <div class="interaction-option  d-flex justify-content-between px-1  py-2 rounded">
+                            <div class="interaction-icon">
+                                <ul class="nav">
+                                    <li class="item d-flex flex-column align-items-center" style="font-size:13px">
+                                        <i class="fa-regular fa-heart fs-5 mx-2 blog-interaction-icons"></i>
+                                        <span class="interaction-count">100</span>
+                                    </li>
+                                    <li class="item d-flex flex-column align-items-center" style="font-size:13px">
+                                        <i class="fa-regular fa-comment fs-5 mx-2 blog-interaction-icons"></i>
+                                        <span class="interaction-count">10</span>
+                                    </li>
+                                    <li class="item d-flex flex-column align-items-center" style="font-size:13px">
+                                        <i class="fa-regular fa-share-from-square fs-5 mx-2 blog-interaction-icons"></i>
+                                    </li>
+                                </ul>
                             </div>
-                            <div class="option d-flex justify-content-between align-items-center">
-                                    <ul class="nav d-flex ms-2" style="gap:15px">
-                                        <li class="blog-option nav-item fs-5"><i class="fa-regular fa-thumbs-up"></i> <span class="count fs-5">10</span></li>
-                                        <li class="blog-option nav-item fs-5"><i class="fa-regular fa-comment"></i> <span class="count fs-5">5</span></li>
-                                        <li class="blog-option nav-item fs-5"><i class="fa-solid fa-share"></i> <span class="count fs-5"></span></li>
-                                    </ul>
-                                    
+                            <div class="visiting-link">
+                                    <i class="fa-solid fa-link fs-5 blog-interaction-icons"></i>
                             </div>
-                    </div>
-                    <div class="card-footer border-top border-secondary position-relative">
-                    <span class="description text-secondary">
-                        Description
-                    </span>
-                    <p class="content my-2" style="text-align: justify" id="blogContent">
-                        <!-- Your initial content goes here -->
-                        In the dynamic realm of business, a proficient business consultant serves as a strategic navigator, offering invaluable insights and solutions. With a keen understanding of market trends and organizational intricacies, they meticulously analyze operations, identify bottlenecks, and prescribe tailored strategies to optimize efficiency and foster growth. Armed with a diverse toolkit of industry knowledge, these consultants function as catalysts for positive change, steering businesses towards sustainable success. Whether it's streamlining processes, enhancing employee engagement, or crafting innovative market entry plans, a skilled business consultant is the linchpin for companies aspiring to navigate the complexities of the corporate landscape with finesse and foresight.
-                        <span class="collapsed-content" style="display: none;"></span>
-                    </p>
-                    <button onclick="toggleReadMore()" id="readMoreBtn" class="btn btn-link" style="display:none;">Read More</button>
-                    <div class="info text-secondary border-top border-secondary px-2">
-                        Posted on <i>January 1, 2023</i> by Bhavesh Parmar
-                    </div>
+                        </div>
+                </div>
+                <div class="card-footer">
+                        <div class="holded-text text-secondary">
+                            Description
+                        </div>
+                        <p class="blog-description-content mt-1" style="text-align:justify">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptate ratione nam accusamus id vel tempora quisquam dolorem eius consequatur. 
+                        </p>
+                        <button class="btn btn-primary">Read More</button>
+
+                        <div class="blog-detail">
+                            <i class="text-secondary" style="font-size:14px">Posted on January 17, 2023 by Bhavesh Parmar </i>
+                        </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <script>
-        var content = document.getElementById('blogContent');
-        var collapsedContent = content.querySelector('.collapsed-content');
-        var readMoreBtn = document.getElementById('readMoreBtn');
-
-        // Check content length
-        if (content.innerText.length > 450) {
-            readMoreBtn.style.display = 'inline';
-            collapsedContent.innerText = content.innerText.substring(450);
-            content.innerText = content.innerText.substring(0, 450);
-        }
-
-        function toggleReadMore() {
-            if (collapsedContent.style.display === 'none') {
-                collapsedContent.style.display = 'inline';
-                readMoreBtn.innerText = 'Read Less';
-            } else {
-                collapsedContent.style.display = 'none';
-                readMoreBtn.innerText = 'Read More';
-            }
-        }
-    </script>
 </body>
 </html>
