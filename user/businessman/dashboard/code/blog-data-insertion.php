f87be28a779189a8bf8e2c38316ce99d84bbf832
connect2local247 <connect2local247@gmail.com>
2024-01-24 19:06:17 +0530

    add blog form designed

---
+++ user/businessman/dashboard/code/blog-data-insertion.php
@@ -0,0 +1,51 @@
+<?php
+            session_start();
+
+            if(isset($_SESSION['error'])){
+                unset($_SESSION['error']);
+            }
+
+            if(isset($_SESSION['greet-message'])){
+                unset($_SESSION['greet-message']);
+            }
+            function is_length_valid($title,$description){
+
+                if (empty($title)) {
+                    $_SESSION['error'] = "Title cannot be empty";
+                    return false;
+                }
+                
+                if (empty($description)) {
+                    $_SESSION['error'] = "Description cannot be empty";
+                    return false;
+                }
+                
+                if (mb_strlen($title) > 50) {
+                    $_SESSION['error'] = "Maximum Length Reached for Title (50 characters limit)";
+                    return false;
+                } 
+                
+                if (mb_strlen($description) > 1000) {
+                    $_SESSION['error'] = "Maximum Length Reached for Description (1000 characters limit)";
+                    return false;
+                }
+                
+                
+                return true;
+            }
+
+            if(isset($_POST['submit'])){
+                $blog_title = $_POST['blog-title'];
+                $blog_description = $_POST['blog-description'];
+
+                $_SESSION['blog-title'] = $blog_title;
+                $_SESSION['blog-description'] = $blog_description;
+
+                if(is_length_valid($blog_title,$blog_description)){
+                    $_SESSION['greet-message'] = "Your Blog Uploaded Successfully.";
+                }
+
+                header("location:/user/businessman/dashboard/form/add-blog.php");
+            }
+
+?>
--- user/businessman/dashboard/form/add-blog.php
+++ user/businessman/dashboard/form/add-blog.php
@@ -1,113 +1,161 @@
+<?php
+      session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Blog</title>
     <link rel="stylesheet" href="/asset/css/form.css">
     <?php include "../../../../asset/link/cdn-link.html"; ?>
-</head>
-<body class="text-bg-dark">
+  </head>
+  <body class="text-bg-dark">
+        <script>
+            path = "/user/businessman/dashboard/dashboard.php";
+        </script>
+        <?php 
 
-            <div class="modal" id="linkModal" tabindex="-1" role="dialog" aria-labelledby="linkModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content text-dark">
-                <div class="modal-header">
-                    <h5 class="modal-title text-dark" id="linkModalLabel">Add Link</h5>
-                    <i class="fa-solid fa-xmark" data-dismiss="modal" aria-label="Close"></i>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group">
-                    <label for="linkTitle">Title:</label>
-                    <input type="text" class="form-control" id="linkTitle">
-                    </div>
-                    <div class="form-group">
-                    <label for="linkUrl">URL:</label>
-                    <input type="text" class="form-control" id="linkUrl">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="addLinkBtn">Add</button>
-                </div>
-                </div>
+                if(isset($_SESSION['greet-message'])){
+                    if(isset($_SESSION['blog-title'])){
+                       $_SESSION['blog-title'] = "";
+                    } 
+                    if(isset($_SESSION['blog-description'])){
+                      $_SESSION['blog-description'] = "";
+                    }
+                }
+
+                include "../../../../component/form-alert.php";
+                unset($_SESSION['error']);
+        ?>
+    <!-- Link Modal -->
+    <div class="modal" id="linkModal" tabindex="-1" role="dialog" aria-labelledby="linkModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content text-bg-light">
+          <div class="modal-header">
+            <h5 class="modal-title" id="linkModalLabel">Add Link</h5>
+            <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button> -->
+            <i class="fa-solid fa-xmark" data-dismiss="modal" arid-hidden="true"></i>
+          </div>
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="linkTitle">Title:</label>
+              <input type="text" class="form-control" placeholder="Enter title here.." id="linkTitle">
             </div>
+            <div class="form-group">
+              <label for="linkUrl">URL:</label>
+              <input type="text" class="form-control" placeholder="Paste your URL here.." id="linkUrl">
             </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="addLinkBtn">Add Link</button>
+          </div>
+        </div>
+      </div>
+    </div>
 
-        <div class="container">
-            <form action="" class="add-blog d-flex justify-content-center p-2">
-                <fieldset class="border rounded col-lg-6 col-md-8 col-12 p-3 position-relative">
-                     <legend class="fw-bold text-center h2 my-3">Add Blog</legend>
-                     <div class="add-link-icon border rounded text-bg-dark bg-gradient shadow p-1 px-2 position-absolute end-0 me-2">
-                        <i class="fa-solid fa-plus" data-toggle="modal" data-target="#linkModal"></i>
-                        
-                     </div>
-                     <div class="blog-content-container p-2 d-flex m-auto ">
-                            <div class="image-container m-auto border p-2 rounded-2 position-relative">
-                                <img src="/asset/image/background/blog.png" class="" style="height:150px;width:150px;" alt="">
-                                <i class="fa-solid fa-camera position-absolute bottom-0 end-0 fs-5 me-1 mb-1 text-white border p-1 rounded bg-dark"></i>
-                            </div>
-                            <div class="video-container d-none">
+    <div class="container">
+      <form action="/user/businessman/dashboard/code/blog-data-insertion.php" method="post" class="add-blog d-flex justify-content-center p-2">
+        <fieldset class="border rounded col-lg-6 col-md-8 col-12 p-3 position-relative">
+          <legend class="fw-bold text-center h2 my-3">Add Blog</legend>
+          <div class="add-link-icon border rounded text-bg-dark bg-gradient shadow p-1 px-2 position-absolute end-0 me-2">
+            <i class="fa-solid fa-plus" data-toggle="modal" data-target="#linkModal" id="#addLinkBtn"></i>
+          </div>
+          <div class="blog-content-container p-2 d-flex m-auto ">
+            <div class="image-container m-auto border p-2 rounded-2 position-relative">
+              <img src="/asset/image/background/blog.png" style="height:150px;width:150px;" alt="">
+              <i class="fa-solid fa-camera position-absolute bottom-0 end-0 fs-5 me-1 mb-1 text-white border p-1 rounded bg-dark"></i>
+            </div>
+          </div>
+          <div class="mt-5">
+            <input type="text" name="blog-title" id="blog-title" class="form-control" placeholder="Blog Title" value="<?php if(isset($_SESSION['blog-title'])) echo $_SESSION['blog-title']; ?>" required>
+          </div>
+          <div class="mt-2">
+            <!-- Video input can be added here if needed -->
+          </div>
+          <div class="mt-2 position-relative">
+            <textarea id="blog-description" name="blog-description" class="form-control position-relative" placeholder="Description" rows="4"><?php if(isset($_SESSION['blog-description'])) echo $_SESSION['blog-description']; ?></textarea>
+            <div id="charCount" class="text-secondary position-absolute bottom-0 end-0 me-2 mb-1">0/1000</div>
+          </div>
+          <div id="addedLinksContainer" class="mt-3 bg-light p-3 rounded" style="display: none;">
+            <!-- This div will contain the added links -->
+          </div>
+          <div class="mt-4 d-flex">
+            <input type="submit" name="submit" class="btn btn-primary px-5 py-2 rounded m-auto" value="Submit">
+          </div>
+        </fieldset>
+      </form>
+    </div>
 
-                            </div>
-                     </div>
-                     <div class="mt-5">
-                         <input type="text" name="blog-title" id="blog-title" class="form-control" placeholder="Blog Title" value="" required>
-                     </div>
-                     <div class="mt-2">
-                            
-                     </div>
-                     <div class="mt-2 position-relative">
-                            <textarea id="blog-description" class="form-control position-relative" placeholder="Description" rows="4"></textarea>
-                            <div id="charCount" class="text-secondary position-absolute bottom-0 end-0 me-2 mb-1">0/1000</div>
-                            
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.1/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <!-- ... (your existing HTML code) ... -->
 
-                            
-                            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.1/dist/umd/popper.min.js"></script>
-                            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-                            <script>
-                             $(document).ready(function() {
+<script>
+  $(document).ready(function() {
     var maxChars = 1000;
+    var linkCount = 0;
+    var displayAddedLinksContainer = document.getElementById('addedLinksContainer');
+    var textarea = $('#blog-description');
 
-    $('#myTextarea').on('input', function() {
+    textarea.on('input', function() {
       var charCount = $(this).val().length;
       $('#charCount').text(charCount + '/' + maxChars);
 
-      if (charCount > maxChars) {
-        $(this).addClass('error');
+      if (charCount >= maxChars) {
+        $(this).addClass('border-danger border-2');
+        // $('.add-link-icon').addClass('error');
+        // showErrorModal();
       } else {
         $(this).removeClass('error');
+        $('.add-link-icon').removeClass('error');
       }
     });
 
+    function showErrorModal() {
+      $('#errorModal').modal('show');
+    }
+
     // Handle Add Link button click
-    $('#addLinkBtn').on('click', function() {
-      var linkTitle = $('#linkTitle').val();
-      var linkUrl = $('#linkUrl').val();
+$('#addLinkBtn').on('click', function() {
+  var linkTitle = $('#linkTitle').val();
+  var linkUrl = $('#linkUrl').val();
+  if (linkUrl != "" && linkTitle != "") {
+    // Create the link HTML
+    var linkHtml = '<a href="' + linkUrl + '" class="text-bg-light d-block p-1 shadow d-flex my-1" style="gap:10px;text-decoration:none;overflow:hidden" target="_blank"><i class="fa-solid fa-link border-end p-1 mx-1" title="' + linkTitle + '"></i> ' + linkUrl + '</a>';
 
-      // Create the link HTML
-      var linkHtml = '<a href="' + linkUrl + '" target="_blank">' + linkTitle + '</a>';
+    // Insert the link at the cursor position in the textarea
+    var cursorPos = textarea[0].selectionStart;
+    var textBefore = textarea.val().substring(0, cursorPos);
+    var textAfter = textarea.val().substring(cursorPos);
+    textarea.val(textBefore + textAfter);
 
-      // Insert the link at the cursor position in the textarea
-      var textarea = $('#myTextarea')[0];
-      var cursorPos = textarea.selectionStart;
-      var textBefore = textarea.value.substring(0, cursorPos);
-      var textAfter = textarea.value.substring(cursorPos);
-      textarea.value = textBefore + linkHtml + textAfter;
+    // Close the modal
+    $('#linkModal').modal('hide');
 
-      // Close the modal
-      $('#linkModal').modal('hide');
-    });
+    // Clear the input fields
+    $('#linkTitle').val('');
+    $('#linkUrl').val('');
+
+    // Display the added link below the textarea
+    displayAddedLinksContainer.style.display = "block";
+    $('#addedLinksContainer').append(linkHtml);
+    linkCount++;
+  } else {
+    alert("Can't be Empty Link Title and URL");
+  }
+});
+
   });
-                            </script>
-                     </div>
-                     <div class="mt-4 d-flex">
-                            <input type="submit" class="btn btn-primary px-5 py-2 rounded m-auto" value="Submit">
-                     </div>
-                </fieldset>
-            </form>
-        </div>
+</script>
 
-      
+<!-- ... (your existing HTML code) ... -->
+
+
+
 </body>
 </html>
